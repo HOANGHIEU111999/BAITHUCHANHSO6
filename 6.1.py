20d25d40f279486f25586a516d781f8662f995a1
HOANGHIEU111999 <50811677+HOANGHIEU111999@users.noreply.github.com>
2019-05-22 09:20:21 +0700

    Update 6.1.py

--- 6.1.py
+++ 6.1.py
@@ -1,3 +1,7 @@
-class Hinhchunhat(a,b):
-a=int(input("nhap a:"))
-b=
+class circle(object):
+    def __init__(self,r):
+        self.radius = r
+    def area (self):
+        return self.radius**2*3.14
+acircle = circle(4)
+print( acircle.area())
